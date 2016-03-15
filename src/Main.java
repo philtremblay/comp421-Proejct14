b2c4e0d68cdb25062a7104cf081a62081eaa7987
Anthony Plescia <anthonyplescia@web092088.wireless.mcgill.ca>
2016-03-15 15:47:03 -0400

    tried class path stuff

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,24 @@
 import javax.swing.*;
+import java.sql.*;
 
 public class Main {
 	
+	private final String JDBC_DRIVER = "com.mysql.jdbc.Driver";  
+	
+	
+	private final String DB_USERNAME = "group14"; //needs to be updated
+	private final String DB_PASSWORD = "[lephant22]";
+
 	
 	public static void main(String[] args)
 	{
 		JFrame lFrame = new JFrame("Group 14");
 		lFrame.setVisible(true);
+	}
+	
+	public void connectToDB()
+	{
+		Class.forName("org.postgresql.Driver");
 	}
 
 }
