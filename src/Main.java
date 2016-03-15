51f95885c6df71955a8ff0300d55d5810db12299
Anthony Plescia <anthonyplescia@web092088.wireless.mcgill.ca>
2016-03-15 16:28:22 -0400

    method cleanup, basic frame generation method

--- src/Main.java
+++ src/Main.java
@@ -8,17 +8,30 @@ public class Main {
 
 	
 	
+	//DB Credentials
 	private static final String DB_USERNAME = "cs421g14"; //needs to be updated
 	private static final String DB_PASSWORD = "[lephant22]";
 	private static final String DB_URL = "jdbc:postgresql://132.206.51.39/cs421";
+	
+	//GUI Elements
+	private static JFrame aFrame;
 
 	
 	public static void main(String[] args)
 	{
-		JFrame lFrame = new JFrame("Group 14");
-		lFrame.setVisible(true);
+		createFrame();
 		connectToDB();
 	}
+	
+	public static JFrame createFrame()
+	{
+		JFrame lFrame = new JFrame("Group 14");
+		lFrame.setSize(512, 512);
+		lFrame.setVisible(true);
+		return lFrame;
+	}
+	
+	
 	
 	public static void connectToDB()
 	{
@@ -36,6 +49,27 @@ public class Main {
 		{
 			System.out.println("No connection");
 		}
+	}
+	
+	public static void executeQuery(Connection pConnection, String pQuery)
+	{
+		assert(pConnection != null);
+		
+		try {
+			Statement lStatement = pConnection.createStatement();
+			ResultSet lResultSet = lStatement.executeQuery(pQuery);
+			while(lResultSet.next())
+			{
+				System.out.println("Column 1 returned");
+				System.out.println(lResultSet.getString(1));
+			}
+			lResultSet.close();
+			lStatement.close();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
 	}
 
 }
